e68c70106b8ba39431a63c09c1b16b9febe6940e
elwdipath <jordan.kaiser@gmail.com>
2020-01-19 12:46:22 -0500

    build basic card function

--- javascript/app.js
+++ javascript/app.js
@@ -18,9 +18,9 @@ var trivia = {
         // two:
             question: "Which vegetable was considered poisonous in Medieval times but was used by the Romans as an aphrodisiac?",
             A:"Tomato",
-            B:"",
+            B:"test",
             C:"t",
-            D:"",
+            D:"test",
             answer: "A",
             // – being in the Solanaceae family, which includes deadly nightshades and other poisonous plants, tomatoes were erroneously thought to be poisonous. According to the folklore, if you would eat a tomato, its poison would turn your blood into acid. Instead, the colonists used to grow tomatoes purely for decoration
             isAsked: false,
@@ -28,10 +28,10 @@ var trivia = {
         {
         // three:
             question:"What is the highest recorded jump by a kangaroo in a single leap?",
-            A:"",
+            A:"test",
             B:"9 meters",
-            C:"",
-            D:"",
+            C:"test",
+            D:"test",
             answer: "B",
                 // (30 feet)
             isAsked: false,
@@ -39,11 +39,11 @@ var trivia = {
         {
         // four: 
             question: "What is the longest movie ever made?",
-            A:"",
-            B:"",
+            A:"test",
+            B:"test",
             C:"Logistics",
-            D:"",
-            answer: "",
+            D:"test",
+            // answer: "",
             // options: ["A. Logistics", "B. Heat", "C. Titanic","D. Saving Private Ryan"],
             //  – at 51,420 minutes, (857 hours or 35 days and 17 hours), it is the longest movie ever made.
             answer: "C",
@@ -52,9 +52,9 @@ var trivia = {
         {
         // five:
             question: "Which country has beaver as the national emblem? Turkey; Peru; Canada",
-            A:"",
-            B:"",
-            C:"",
+            A:"test",
+            B:"test",
+            C:"test",
             D:"Canada",
             answer: "D",
             //
@@ -65,19 +65,19 @@ var trivia = {
         // six: 
             question:"Which cells are situated only within the nervous system?",
             A:"Neurons",
-            B:"",
-            C:"",
-            D:"",
+            B:"test",
+            C:"test",
+            D:"test",
             answer: "A",
             // – humans have around 100 billion neurons in their brain alone.
             isAsked: false
         },{ 
         // seven:
             question: "Which is the hottest planet? Venus; Mercury; Saturn; Jupiter",
-            A:"",
-            B:"",
+            A:"test",
+            B:"test",
             C:"Venus",
-            D:"",
+            D:"test",
             answer: "C",
             // – although Mercury is much closer to the Sun, it has no atmosphere. The atmosphere on Venus helps retain much more of the Sun’s heat.
             isAsked: false
@@ -85,17 +85,17 @@ var trivia = {
         // eight: 
             question:"Which war caused the greatest loss of life for Americans? WW1; WW2; Vietnam; The American Civil War",
             A:"The American Civil War",
-            B:"",
-            C:"",
-            D:"",
+            B:"test",
+            C:"test",
+            D:"test",
             answer: "A",
             // – around 620,000 dead. This is more than WW1, WW2 and Vietnam combined.
             isAsked: false
         }, {
         // nine:
             question:"Which continent covers the largest area? Asia; North America; Africa; South America",
-            A:"",
-            B:"",
+            A:"test",
+            B:"test",
             C:"t",
             D:"Asia",
             answer: "D",
@@ -104,8 +104,8 @@ var trivia = {
         },{ 
         // ten:
             question:"Han, Ming and Qing were ruling dynasties in which country?",
-            A:"",
-            B:"",
+            A:"test",
+            B:"test",
             C:"t",
             D:"China",
             answer: "D",
@@ -123,34 +123,30 @@ var trivia = {
 let timer;
 let curQ;
 
-
+//3. Grab a random object from game.questionArr 
 function rQuestion(){
     
     var q = trivia.questionsArr[Math.floor(Math.random() * trivia.questionsArr.length)]
     if (q.isAsked === false){
-        console.log(q);
+        // console.log(q);
         curQ = q
         q.isAsked = true;
-    }
+    }//TODO!: Else statement when all questions are set to true
 
 }
+//display Q on the screen.
 
+function buildCard(){
 
-// for (var i = 0; i < trivia.questionsArr.length; i++){
-//     // var question = trivia.questionsArr[i].question;
-//     // var answer = trivia.questionsArr[i].answer
-//     // alert(question)
-    
-//     $("#question").text(trivia.questionsArr[i].question)
-//     $("#options").text(trivia.questionsArr[i].optionA)
+    $("#question").html('<h3>'+curQ.question+'</h3>')
+    $("#A").html('<input type = "radio" id = "A">' + curQ.A)
+    $("#B").html('<input type = "radio" id = "B">' + curQ.B)
+    $("#C").html('<input type = "radio" id = "C">' + curQ.C)
+    $("#D").html('<input type = "radio" id = "D">' + curQ.D)
+}
+rQuestion()
+buildCard()
 
-//     };
-
-
-
-
-//2. Create questions and set them in an array in the game object
-//3. Grab a random object from game.questionArr and display it on the screen.
 //4. Start a timer of 30 secs and display question
     //4a. if question is answered correctly display correct text and increment game.correctAnswers by 1
     //4b. if answer is incorrect display incorrect text and increment game.wrongAnswers by 1
